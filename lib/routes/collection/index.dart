f1ce1ab31b5e455b3f6f0beb4755803bcc2d58e8
skyinJune <215933471@qq.com>
2020-12-30 00:30:41 +0800

    bottom bar 初探索

---
+++ lib/routes/collection/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CollectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('我的收藏'),
+      ),
+      body: Center(
+        child: Text('收藏'),
+      ),
+    );
+  }
+}
--- lib/routes/home/index.dart
+++ lib/routes/home/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_flutter/routes/collection/index.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -9,6 +10,36 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Text('首页'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          children: [
+            IconButton(
+                icon: Icon(Icons.calendar_today),
+                onPressed: () {
+                  print('1');
+                }),
+            IconButton(
+                icon: Icon(Icons.star),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return CollectionPage();
+                  }));
+                }),
+            IconButton(
+                icon: Icon(Icons.explore),
+                onPressed: () {
+                  print('1');
+                }),
+            IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () {
+                  print('1');
+                }),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        ),
       ),
     );
   }
