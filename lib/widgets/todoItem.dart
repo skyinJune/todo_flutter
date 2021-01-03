841f1f4b2bf63694ed539b9a292e26be89a0e4ca
skyinJune <215933471@qq.com>
2021-01-03 23:42:00 +0800

    使用expanded弹性布局todoItem

--- lib/widgets/todoItem.dart
+++ lib/widgets/todoItem.dart
@@ -14,7 +14,30 @@ class _TodoItemState extends State<TodoItem> {
           borderRadius: BorderRadius.all(Radius.circular(5.0))),
       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 17),
       child: Row(
-        children: [Chip(label: Text('A')), Text('标题')],
+        children: [
+          Chip(label: Text('A')),
+          Expanded(
+              flex: 5,
+              child: Container(
+                color: Theme.of(context).primaryColor,
+                alignment: Alignment.center,
+                child: Text('标题已经'),
+              )),
+          Expanded(
+              flex: 3,
+              child: Container(
+                color: Theme.of(context).accentColor,
+                alignment: Alignment.center,
+                child: Text('10'),
+              )),
+          Expanded(
+              flex: 2,
+              child: Container(
+                color: Theme.of(context).cardColor,
+                alignment: Alignment.center,
+                child: Text('天'),
+              )),
+        ],
       ),
       padding: EdgeInsets.all(20),
     );
